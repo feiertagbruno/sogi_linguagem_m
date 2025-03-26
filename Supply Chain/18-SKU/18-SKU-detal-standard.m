2121c4dc2006a9ae6a5d3b73ebc29047ff571dd6
Bruno Feiertag Martini <feiertagbruno@gmail.com>
2025-03-26 08:43:21 -0400

    sogi antes da adaptação do detalhamento

--- Supply Chain/18-SKU/18-SKU-detal-standard.m
+++ Supply Chain/18-SKU/18-SKU-detal-standard.m
@@ -3,20 +3,31 @@ let
     detalhamento_standard_Table = Fonte{[Item="detalhamento_standard",Kind="Table"]}[Data],
 	
 	colunas_necessarias = Table.SelectColumns(detalhamento_standard_Table,{
-		"COD PA","CODIGO","QTDE.NECESSARIA","Custo c/ Premissa"
+		"COD PA","CODIGO","QTDE.NECESSARIA","Custo c/ Premissa","DESCRICAO"
 	}),
 
     tipo_alterado = Table.TransformColumnTypes(colunas_necessarias,{
 		{"COD PA", type text}, {"CODIGO", type text}, 
-		{"QTDE.NECESSARIA", type number}, {"Custo c/ Premissa", type number}
+		{"QTDE.NECESSARIA", type number}, {"Custo c/ Premissa", type number},
+		{"DESCRICAO", type text}
 	}),
+	buffer = Table.Buffer(tipo_alterado),
 
-	relacao = Table.AddColumn(tipo_alterado, "relacao", each [COD PA] & "-" & [CODIGO], type text),
-
-	group = Table.Group(relacao,{"COD PA","CODIGO","relacao"},{
+	group = Table.Group(buffer,{"COD PA","CODIGO"},{
 		{"QTDE.NECESSARIA", each List.Sum([#"QTDE.NECESSARIA"]), type number},
 		{"Custo c/ Premissa", each List.Sum([#"Custo c/ Premissa"]), type number}
-	})
+	}),
+	buffer_2 = Table.Buffer(group),
+
+	group_por_CODIGO = Table.Group(buffer,"CODIGO",{"DESCRICAO", each List.First([DESCRICAO]), type text}),
+
+	buffer_descricao = Table.Buffer(group_por_CODIGO),
+
+	traz_descricoes = Table.ExpandTableColumn(
+		Table.NestedJoin(buffer_2,{"CODIGO"}, buffer_descricao,{"CODIGO"},"dados", JoinKind.LeftOuter)
+		,"dados",{"DESCRICAO"}
+	)
+
 
 in
-    group
+    traz_descricoes
--- Supply Chain/18-SKU/18-SKU-detalhamento.m
+++ Supply Chain/18-SKU/18-SKU-detalhamento.m
@@ -1,55 +1,117 @@
 let
-    Fonte_tab = Excel.Workbook(File.Contents("C:\Users\bruno.martini\Desktop\Multi Estruturas Sem Frete.xlsx"), null, true),
+    Fonte_tab = Excel.Workbook(File.Contents("M:\SOGI\2025\BI-SOGI\Bases\Custo dos SKUs\Multi Estruturas Sem Frete.xlsx"), null, true),
     det_table = Fonte_tab{[Item="detalhamento",Kind="Table"]}[Data],
 
     transf_data = Table.TransformColumnTypes(det_table,{"Data de Referência", type date}),
 
-    menor_data = List.Min(Table.Column(transf_data,"Data de Referência")),
-    maior_data = Date.AddMonths(List.Max(Table.Column(transf_data,"Data de Referência")),-2),
-
-    reduz_colunas = Table.SelectRows(transf_data, each [Data de Referência] >= maior_data or [Data de Referência] = menor_data),
-
-    colunas_necessarias = Table.SelectColumns(reduz_colunas,{
+    colunas_necessarias = Table.SelectColumns(transf_data,{
         "Cód Original","Desc Orig","Insumo","Descrição Insumo","Quant Utilizada",
-        "Últ Fechamento","Data de Referência","Últ Compra com Premissa"
+        "Data de Referência","Últ Compra com Premissa"
     }),
 
     transforma_tipos = Table.TransformColumnTypes(colunas_necessarias,{
         {"Data de Referência", type date},{"Cód Original", type text},{"Desc Orig",type text},
         {"Insumo", type text},{"Descrição Insumo", type text}, {"Quant Utilizada",type number},
-        {"Últ Fechamento", type number},{"Últ Compra com Premissa", type number}
+        {"Últ Compra com Premissa", type number}
     }),
 
     ano_num = Table.AddColumn(transforma_tipos,"ano_num", each Date.Year([#"Data de Referência"]), Int16.Type),
 
 	mes_num = Table.AddColumn(ano_num,"mes_num", each [ano_num] * 100 + Date.Month([Data de Referência]), Int32.Type),
 
-    col_meses = Table.Column(mes_num, "mes_num"),
-    
-    menor_mes = List.Min(col_meses),
-    maiores_3_meses = List.FirstN( List.Sort( List.Distinct( col_meses ) ,Order.Descending) ,3),
-    maior_mes = List.Max(col_meses),
+    remover_cols = Table.RemoveColumns(mes_num,{"Data de Referência","Descrição Insumo"}),
 
-    meses_a_considerar = Table.TransformColumnTypes(
-        Table.FromList(
-            List.Combine({{menor_mes}, maiores_3_meses})
-            ,Splitter.SplitByNothing(),{"meses_a_considerar"}
-        )
-        ,{"meses_a_considerar", Int32.Type}
+    group_por_mes_num = Table.Group(remover_cols,{"mes_num","ano_num"},{
+        "group",each _, type table[
+            #"Cód Original" = Text.Type, #"Desc Orig" = Text.Type, Insumo = Text.Type,
+            #"Quant Utilizada" = Number.Type,
+            #"Últ Compra com Premissa" = Number.Type
+        ]
+    }),
+
+    trazer_comparacao_standard = Table.TransformColumns(group_por_mes_num,{
+        {"group", (tb) => (
+            let
+                traz_std = Table.ExpandTableColumn(
+                    Table.NestedJoin(tb,{"Cód Original","Insumo"},#"18-SKU-detal-standard",{"COD PA","CODIGO"},"dados",JoinKind.FullOuter)
+                    ,"dados", {"COD PA","CODIGO","QTDE.NECESSARIA", "Custo c/ Premissa","DESCRICAO"}, 
+                    {"COD PA","CODIGO","qtd_necess_std","custo_std","DESCRICAO"}
+                )
+            in
+                traz_std
+        )}
+    }),
+    expandir = Table.ExpandTableColumn(
+        trazer_comparacao_standard,"group",{
+            "Cód Original","Desc Orig","Insumo","Quant Utilizada",
+            "Últ Compra com Premissa",
+            "COD PA","CODIGO","qtd_necess_std","custo_std","DESCRICAO"
+        }
     ),
 
-    filtra_meses_a_considerar = Table.RemoveColumns(
-        Table.NestedJoin(
-            mes_num,"mes_num",meses_a_considerar,"meses_a_considerar","dados",JoinKind.Inner
-        ), "dados"
-    ),
+    cols_mes_num = Table.Column(expandir,"mes_num"),
+    maior_mes = List.Max(cols_mes_num),
+    menor_mes = List.Min(cols_mes_num),
 
-    relacao = Table.AddColumn(filtra_meses_a_considerar,"relacao",each [Cód Original] & "-" & [Insumo], type text),
+    mes_filtro = Table.AddColumn(expandir,"mes_filtro", each (
+        if [mes_num] = menor_mes then "Real WK42/24" else
+        if [mes_num] = maior_mes then "Último Mês" else "Mês Anterior"
+    ), type text),
 
-    mes_filtro = Table.AddColumn(relacao,"mes_filtro", each (
-        if [mes_num] = menor_mes then "Standard" else
-        if [mes_num] = maior_mes then "Último Mês" else Date.ToText([Data de Referência], "MMM/yy")
-    ), type text)
+    mes_filtro_buffer = Table.Buffer(mes_filtro),
+
+    group_desc_orig = Table.Group(mes_filtro_buffer,"Cód Original",{"Desc Orig", each List.First([Desc Orig]), type text}),
+    group_desc_orig_buffer = Table.Buffer(group_desc_orig),
+
+    completa_col_PA = Table.RenameColumns((
+        Table.RemoveColumns((
+            Table.AddColumn(mes_filtro_buffer,"Cód Original temp",each (
+                if [Cód Original] = null then [COD PA] else [Cód Original]
+            ), type text)
+        ),{"Cód Original"})
+    ),{"Cód Original temp","Cód Original"}),
+
+    // completa_descricao_PA = Table.ExpandTableColumn(
+    //     Table.NestedJoin((
+    //         Table.RemoveColumns(completa_col_PA,{"Desc Orig"})
+    //     ),"Cód Original",group_desc_orig_buffer,"Cód Original", "dados", JoinKind.LeftOuter)
+    //     ,"dados",{"Desc Orig"}
+    // ),
+
+    completa_insumo = Table.RenameColumns((
+        Table.RemoveColumns((
+            Table.AddColumn(completa_col_PA,"Insumo temp", each (
+                if [Insumo] = null then [CODIGO] else [Insumo]
+            ), type text)
+        ),{"Insumo"})
+    ),{"Insumo temp", "Insumo"}),
+
+    remover_colunas_desn = Table.RemoveColumns(completa_insumo,{
+        "ano_num","COD PA","CODIGO","DESCRICAO","Desc Orig"
+    }),
+
+    // group feito para unificar linhas que poderiam estar duplicadas (somente para garantia)
+    group_pre_pareto = Table.Group(remover_colunas_desn,
+        {"Cód Original","Insumo","mes_num","mes_filtro"},{
+        {"Quant Utilizada", each Number.Round(List.Sum([Quant Utilizada]),6), type number},
+        {"Últ Compra com Premissa", each Number.Round(List.Sum([Últ Compra com Premissa]),5), type number},
+        {"qtd_necess_std", each Number.Round(List.Sum([qtd_necess_std]),6), type number},
+        {"custo_std", each Number.Round(List.Sum([custo_std]),5), type number}
+    }),
+
+    group_pareto = Table.Group(group_pre_pareto,{"Cód Original","mes_num","mes_filtro"}, {
+        {"group", each _, type table}
+    }),
+
+    calcula_paretos = Table.TransformColumns(group_pareto,{
+        {"group", (tb) => (
+            let
+                
+            in
+        ), type table}
+    })
+
+
 
 in
-    mes_filtro
+    group_pareto
